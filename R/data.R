bfe45ad54e161906a7e710fa43636891ea22b952
Joel Gombin <joel.gombin@gmail.com>
2015-09-12 18:21:33 +0200

    data documentation

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Socioprofessionnal making of the population over 18 in Paris
+#'
+#' A dataset containing the number of inhabitants by socioprofessional category in 2011 in Paris, by IRIS (the French census unit).
+#'
+#' @source [INSEE](http://www.insee.fr/fr/themes/detail.asp?reg_id=99&ref_id=infra-population-11)
+"RP_2011_CS8_Paris"
+
+#' Paris IRIS shapefile, 2013
+#'
+#' A shapefile of the IRIS (French census unit) in Paris.
+#'
+#' @source [IGN](http://professionnels.ign.fr/contoursiris)
+"ParisIris"
+
+#' Paris polling stations shapefile, 2012
+#'
+#' A shapefile of the polling stations in Paris in 2012.
+#'
+#' @source [Paris city](http://opendata.paris.fr/explore/dataset/zones-de-rattachement-des-bureaux-de-vote-en-2012/?tab=table)
+"ParisPollingStations2012"
